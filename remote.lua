095de066f5b2a69ebffe009d1755f62313f20aa0
Too_young <43996453+excited233@users.noreply.github.com>
2020-05-07 12:47:37 +0800

    Update remote.lua

--- remote.lua
+++ remote.lua
@@ -374,7 +374,7 @@ import "com.my.sc.*"
           };
           {
             TextView;
-            text=Html.fromHtml("免费喝咖啡☕️ <small>童叟无欺</>");
+            text=Html.fromHtml("免费喝咖啡 <small><i>快上车</></>");
             textColor=内容颜色;
             layout_marginLeft="10sp";
             textSize="16sp";
